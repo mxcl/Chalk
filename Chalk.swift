ecab9b566691b19205ea92c44fcb650f2075b577
Max Howell <mxcl@me.com>
2019-02-20 22:02:56 -0500

    Other styles

--- Chalk.swift
+++ Chalk.swift
@@ -9,16 +9,19 @@ public enum Color: Int {
     case white = 37
 }
 
-public enum Style {
-    case bold
+public enum Style: Int {
+    case bold = 1
+    case underline = 4
+    case italic = 3
+    case strikethrough = 9  // not implemented in Terminal.app
 }
 
 public extension String.StringInterpolation {
     mutating func appendInterpolation(_ text: String, color: Color, style: Style? = nil) {
         appendLiteral("\u{001B}[")
         appendLiteral(String(color.rawValue))
-        if style == .bold {
-            appendLiteral(";1")
+        if let style = style?.rawValue {
+            appendInterpolation(";\(style)")
         }
         appendLiteral("m")
         appendLiteral(text)
