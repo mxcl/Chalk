693d4c54b2aaa7f5dcc1be0678047e805fb5a535
Max Howell <mxcl@me.com>
2019-03-30 19:07:38 -0400

    Tidy

--- Chalk.swift
+++ Chalk.swift
@@ -11,34 +11,34 @@ public enum Color: RawRepresentable {
     
     public init?(rawValue: UInt8) {
         switch rawValue {
-        case 0: self = .black
-        case 1: self = .red
-        case 2: self = .green
-        case 3: self = .yellow
-        case 4: self = .blue
-        case 5: self = .magenta
-        case 6: self = .cyan
-        case 7: self = .white
-        default: self = .extended(rawValue)
+            case 0: self = .black
+            case 1: self = .red
+            case 2: self = .green
+            case 3: self = .yellow
+            case 4: self = .blue
+            case 5: self = .magenta
+            case 6: self = .cyan
+            case 7: self = .white
+            default: self = .extended(rawValue)
         }
     }
     
     public var rawValue: UInt8 {
         switch self {
-        case .black: return 0
-        case .red: return 1
-        case .green: return 2
-        case .yellow: return 3
-        case .blue: return 4
-        case .magenta: return 5
-        case .cyan: return 6
-        case .white: return 7
-        case .extended(let number): return number
+            case .black: return 0
+            case .red: return 1
+            case .green: return 2
+            case .yellow: return 3
+            case .blue: return 4
+            case .magenta: return 5
+            case .cyan: return 6
+            case .white: return 7
+            case .extended(let number): return number
         }
     }
 }
 
-public struct Style: OptionSet, Hashable {
+public struct Style: OptionSet {
     public static let bold = Style(rawValue: 1 << 0)
     public static let dim = Style(rawValue: 1 << 1)
     public static let italic = Style(rawValue: 1 << 2)
